519b8147308c6348add5a42765fb3a241aae2601
Artem Klepcha <woullbors96@yandex.ru>
2018-05-11 20:15:30 +0300

    part 1

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    public float speed;
+
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = rigidbody.transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+    public Boundary boundary;
+    public float tilt;
+    Rigidbody rigidbody;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.5f;
+    public float nextFire = 0.0f;
+
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("MyFire") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //GameObject clone
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); //as GameObject
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rigidbody.rotation = Quaternion.Euler(0f, 0f, rigidbody.velocity.x*-tilt);
+        rigidbody.velocity = new Vector3(moveHorizontal, 0, moveVertical)*speed;
+        rigidbody.position = new Vector3
+            (
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0f,
+            Mathf.Clamp(rigidbody.position.z,boundary.zMin,boundary.zMax)
+            );
+    }
+}
